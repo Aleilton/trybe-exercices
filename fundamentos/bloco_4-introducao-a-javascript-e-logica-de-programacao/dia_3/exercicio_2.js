0da8a0e87f9346203a6e73cec0c6a1e2fb7bde87
Aleilton Chavenco <aleilton@gmail.com>
2021-05-21 18:23:00 -0300

    Exercício 2

---
+++ fundamentos/bloco_4-introducao-a-javascript-e-logica-de-programacao/dia_3/exercicio_2.js
@@ -0,0 +1,17 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// n = 5
+// *
+// **
+// ***
+// ****
+// *****
+
+let n = 20;
+
+for (let index = 0; index < n; index += 1) {
+  let auxiliar = '';
+  for (let indexSec = 0; indexSec < index; indexSec += 1) {
+    auxiliar = auxiliar + '*';
+  }
+  console.log(auxiliar);
+}
