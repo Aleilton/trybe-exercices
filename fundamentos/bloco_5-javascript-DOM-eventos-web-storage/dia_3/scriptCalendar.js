9bc5268c9a3370262569614d4d777126c25cfe44
Aleilton Chavenco <aleilton@gmail.com>
2021-05-31 19:14:19 -0300

    Exercício 2

--- fundamentos/bloco_5-javascript-DOM-eventos-web-storage/dia_3/scriptCalendar.js
+++ fundamentos/bloco_5-javascript-DOM-eventos-web-storage/dia_3/scriptCalendar.js
@@ -6,12 +6,22 @@ function createDaysOfTheWeek() {
     const days = weekDays[index];
     const dayListItem = document.createElement('li');
     dayListItem.innerHTML = days;
-
+    
     weekDaysList.appendChild(dayListItem);
   };
 };
 
 createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+
+// Exercício 1:
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+// Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+// Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+// const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const holidayList = [24, 25, 31];
@@ -19,13 +29,13 @@ const fridayList = [4, 11, 18, 25];
 
 function createCalendar(diasDoMes) {
   const daysList = document.querySelector('#days');
-
+  
   for (let index = 0; index < diasDoMes.length; index += 1) {
     const day = diasDoMes[index];
     let tipoDia = 'day ';
     const dayListItem = document.createElement('li');
     dayListItem.innerHTML = day;
-
+    
     let isFriday = verificaSexta(day);
     
     if (verificaFeriado(day) == true) {
@@ -35,7 +45,7 @@ function createCalendar(diasDoMes) {
       tipoDia += 'friday ';
     }
     dayListItem.className = tipoDia;
-
+    
     daysList.appendChild(dayListItem);
   }
 }
@@ -63,4 +73,18 @@ function verificaSexta(dia) {
 }
 
 createCalendar(dezDaysList);
-// Escreva seu código abaixo.
+
+// Exercício 2:
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+let buttonsContainer = document.querySelector('.buttons-container');
+
+function createBtnHoliday (nome) {
+  let button = document.createElement('button');
+  button.innerText = nome;
+  button.id = 'btn-holiday';
+  buttonsContainer.appendChild(button);
+}
+
+createBtnHoliday('Feriados');
