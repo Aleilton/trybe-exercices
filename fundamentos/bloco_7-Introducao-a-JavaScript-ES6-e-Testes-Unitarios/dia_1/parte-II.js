8eeb014333841683fe7657b4fe000e33d1bed0a0
Aleilton Chavenco <aleilton@gmail.com>
2021-06-17 18:42:48 -0300

    Exercício Parte II - Exercícios 1 e 2

---
+++ fundamentos/bloco_7-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/dia_1/parte-II.js
@@ -0,0 +1,21 @@
+const fatorial = number => {
+  let result = 1;
+  for (let index = number; index > 0; index -= 1) {
+    result *= index;
+  }
+  return result;
+};
+
+console.log(fatorial(4));
+
+
+const longestWord = frase => {
+  let words = frase.split(' ');
+  let biggerWord = words[0];
+  for (let index = 0; index < words.length; index += 1){
+    biggerWord = biggerWord.length < words[index].length ? words[index] : biggerWord;
+  }
+  console.log(biggerWord);
+}
+
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu agora") // retorna 'aconteceu'
