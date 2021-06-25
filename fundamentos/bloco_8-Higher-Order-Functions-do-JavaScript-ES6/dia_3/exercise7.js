77d09641d12859485e441d91ec8e4411f4e2062a
Aleilton Chavenco <aleilton@gmail.com>
2021-06-25 19:14:42 -0300

    Exercício 7

--- fundamentos/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_3/exercise7.js
+++ fundamentos/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_3/exercise7.js
@@ -64,4 +64,13 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
-//
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  // escreva seu código aqui
+  return books.find((book) => book.author.name[1] === '.' && book.author.name[4] === '.' && book.author.name[7] === '.').name;
+}
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
