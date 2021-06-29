45bb6d2f8254cdcee3488cdb8ae288127cd01273
Aleilton Chavenco <aleilton@gmail.com>
2021-06-29 19:01:28 -0300

    Exercício 6

--- fundamentos/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_5/exercise5.js
+++ fundamentos/bloco_8-Higher-Order-Functions-do-JavaScript-ES6/dia_5/exercise5.js
@@ -7,7 +7,7 @@ const myList = [1, 2, 3];
 
 // escreva swap abaixo
 
-const swap = ([first, second, third]) => [ third, second, first];
+const swap = ([first, second, third]) => [third, second, first];
 
 const swappedList = swap(myList);
 
